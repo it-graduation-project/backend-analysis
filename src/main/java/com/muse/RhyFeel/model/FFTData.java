150426799f7b7e629ba1c70395b122211b5038e5
dankim-dev <daninld042@gmail.com>
2025-03-11 18:48:03 +0900

    comment: FFTData 주석 정리

--- src/main/java/com/muse/RhyFeel/model/FFTData.java
+++ src/main/java/com/muse/RhyFeel/model/FFTData.java
@@ -1,24 +1,3 @@
-//package com.muse.RhyFeel.model;
-//
-//import jakarta.persistence.*;
-//import lombok.Getter;
-//import lombok.Setter;
-
-//@Entity
-//@Table(name = "fft_data")
-//@Getter
-//@Setter
-//public class FFTData {
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    private Long fftId;
-//
-//    @Column(nullable = false)
-//    private Long musicId;
-//
-//    @Column(name = "frequency_data", columnDefinition = "TEXT", nullable = false)
-//    private String frequencyData; // JSON 형식으로 저장
-//}
 package com.muse.RhyFeel.model;
 
 import jakarta.persistence.*;
